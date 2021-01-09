73ce4f840f27995f119afbe7e22f031ce8630b2d
E2255-Nuray <nurayg76@gmail.com>
2021-01-09 13:30:40 +0000

    added scripts for qa automation environment

--- jenkins/push-dev-docker-images-to-ecr.sh
+++ jenkins/push-dev-docker-images-to-ecr.sh
@@ -1,4 +1,3 @@
-# Provide credentials for Docker to login the AWS ECR and push the images
 aws ecr get-login-password --region ${AWS_REGION} | docker login --username AWS --password-stdin ${ECR_REGISTRY} 
 docker push "${IMAGE_TAG_ADMIN_SERVER}"
 docker push "${IMAGE_TAG_API_GATEWAY}"
